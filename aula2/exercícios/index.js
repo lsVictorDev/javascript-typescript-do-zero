288751de20c2a0d57fef85881de3258e54838dcc
Victor Lemos <132801215+lsVictorDev@users.noreply.github.com>
2026-01-17 22:45:51 -0300

    exercício básico de concatenação no console
    Adiciona exercício introdutório utilizando console.log para exibir
    uma frase combinando strings e valores numéricos, reforçando conceitos básicos de lógica de programação sem uso de variáveis.

---
+++ aula2/exercícios/index.js
@@ -0,0 +1,8 @@
+// Primeiro exercício
+// Mostrar a seguinte frase:
+// "Meu nome é "NOME". Estou aprendendo JavaScript às 21 da noite."
+// obs: O horario deve ser um "number" e não um "string".
+
+console.log('Meu nome é "Victor". Estou aprendendo JavaScript às', 21 ,'da noite.');
+
+// exercício básico feito ainda sem variável
