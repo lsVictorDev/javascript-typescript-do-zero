5a0595d90981b647c15d2502030c9d239977dc44
Victor Lemos <132801215+lsVictorDev@users.noreply.github.com>
2026-01-17 22:41:49 -0300

    Primeira aula de lógica em JavaScript
    Adiciona exemplos iniciais de lógica de programação em JavaScript, abordando diferentes formas de exibição de strings e números no console. Conteúdo introdutório com foco na revisão dos fundamentos da linguagem.

---
+++ aula1/index.js
@@ -0,0 +1,12 @@
+// Lógica de programação em JavaScript
+// Mesmo já entendendo a lógica de programação, optei por começar do zero
+// para revisar os conceitos fundamentais da linguagem.
+
+// Formas de exibir um texto (string)
+console.log('Meu nome é "Victor"'); // String entre aspas simples
+console.log("Meu nome é 'Victor'"); // String entre aspas dupla
+console.log(`Meu "nome" é 'Victor'`); // String entre Crase
+
+// Formas de exibir valores numéricos (number)
+console.log(35) // Number
+console.log(15.22) // Número de ponto flutuante
