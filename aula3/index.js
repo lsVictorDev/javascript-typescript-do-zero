f90521d066af7e4e69728f8b716e53e62b4f7062
Victor Lemos <132801215+lsVictorDev@users.noreply.github.com>
2026-01-17 22:48:35 -0300

    Introdução aos comentários em JavaScript
    Adiciona exemplos de comentários de linha e comentários de bloco, demonstrando como documentar e desativar trechos de código em JavaScript durante o desenvolvimento.

---
+++ aula3/index.js
@@ -0,0 +1,10 @@
+// Aprendendo a usar comentário no código
+
+// Este é um comentário de linha:
+// console.log('Olá mundo!');
+
+/* Este é um comentário de bloco
+console.log('Linha 1');
+console.log('Linha 2'); 
+console.log('Linha 3');
+*/
