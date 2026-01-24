000f45353c557410b481033a7300b6eb4d6e1258
Victor Lemos <132801215+lsVictorDev@users.noreply.github.com>
2026-01-23 23:50:40 -0300

    introdução aos tipos de dados primitivos
    Exemplos de string, number, boolean, null e undefined, Diferença conceitual entre null e undefined e Declaração e comportamento de variáveis primitivas.

---
+++ aula9/index.js
@@ -0,0 +1,39 @@
+// Tipos de dados primitivos
+
+// String
+const nome1 = 'victor' // String
+const nome2 = "Flávia" // String
+const nome3 = `Fabricio` // String com crase podemos usar templates strings
+
+// Number
+const num1 = 10; // Number
+const num2 = 20.14; // Number de ponto flutuante
+
+// variável indefinida
+let nomeAluno; // undefined - não aponta para local nenhuma na memória, apenas foi declarada.
+nomeAluno = 23;
+console.log(nomeAluno);
+
+// Null
+let sobrenomeAluno = null; // Nulo - não aponta para local nenhuma na memória.
+
+// diferença de null para undefined
+// Representa a ausência intencional de valor.
+// É utilizado quando sabemos que a variável irá receber um valor posteriormente,
+// mas, naquele momento, ela ainda não possui nenhum.
+// Exemplo prático: um botão que altera a cor de fundo do site.
+// A variável pode iniciar como null e, após a ação do usuário,
+// receber um valor como "vermelho".
+
+// undefined:
+// Representa uma variável que foi declarada, mas ainda não recebeu nenhum valor.
+// O próprio JavaScript atribui undefined automaticamente.
+// Também ocorre quando tentamos acessar uma propriedade ou retorno de função
+// que não existe ou não foi definido.
+
+// Boolean
+// O tipo de variável boolean é a que recebe dois tipos de valores, true ou false.
+
+const aprovado = true; 
+const boolean = false;
+
